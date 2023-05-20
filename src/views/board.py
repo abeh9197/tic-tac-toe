611e8b2a74302c230476a7433fbbd7fad7df0cf4
Hiroyuki Abe <abeh9197@gmail.com>
2023-05-20 12:45:02 +0900

    fix: board view.

--- src/views/board.py
+++ src/views/board.py
@@ -1,1 +1,13 @@
-def print_board(board: )
+from models.board import Board
+
+
+def print_current(board: Board) -> None:
+    """
+    Print current board on terminal.
+    """
+    print("+---+---+---+")
+    for i, row in enumerate(board):
+        print("|", row[0], "|", row[1], "|", row[2], "|")
+        if i != 2:
+            print("+---+---+---+")
+    print("+---+---+---+")
